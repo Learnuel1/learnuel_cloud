d12089d5b815337b619b240038b9fc3f8ce14e55
isaacsolomon332-cell <isaacsolomon332@gmail.com>
2025-12-05 03:52:40 -1100

    login

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { CONFIG } = require("./src/config");
-const { ENV_CONFIG } = require("./src/config/env");
 const server = express();
+const {CONFIG} = require("./src/config")
 const AppRouter = require("./src/routes");
+const connectDB = require("./src/config/db");
+const { ENV_CONFIG } = require("./src/config/env");
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
@@ -13,6 +14,9 @@ server.use("/api/v1", AppRouter);
 
 server.listen(ENV_CONFIG.PORT, async () => {
     try {
+        console.log("connecting to database...");
+        
+        await connectDB();
         console.log(`${CONFIG.APP_NAME} is running on port http://localhost:${ENV_CONFIG.PORT}`);
     } catch (error) {
         console.log(error);
