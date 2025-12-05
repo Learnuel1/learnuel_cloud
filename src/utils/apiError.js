d72eff9d114c97d9b0510d15cdb8bceac9430872
= <=>
2025-12-05 16:00:43 +0100

    fix: correct the account schema

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,13 +1,20 @@
 class APIError extends Error {
-    constructor(message, statusCode) {
+    constructor(message, status) {
         super(message);
-        this.statusCode = statusCode; 
+        this.status = status; 
     }  
-    static notFound(message = "Resource not found", statusCode = 404) {
+    static notFound(message = "Resource not found", status = 404) {
         return new this(message, 404);
     }
-    static badRequest(message = "Bad request", statusCode = 400) {
+    static badRequest(message = "Bad request", status = 400) {
         return new this(message, 400);
     }
-    
+     static unauthorized(message, status = 403) {
+    const message = message || `You don't have required permission`;
+    return new this(message, status);
+  }
+  static unauthenticated(message, status = 401) {
+    const message = message || `You need to login first in order to have access.`;
+    return new this(message, status);
+  }
 }
